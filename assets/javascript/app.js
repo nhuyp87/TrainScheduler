f436af0c0ec7e55f1c2550ae1a478da919957aff
nhuyp <ypham87@gmail.com>
2017-05-06 11:28:38 -0700

    Append data to files

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -12,7 +12,7 @@
 
   var database = firebase.database();
 
-// 2. Button for adding Employees
+// Button for adding train data
 $("#add-trainTime-btn").on("click", function(event) {
   event.preventDefault();
 
@@ -47,6 +47,37 @@ $("#add-trainTime-btn").on("click", function(event) {
 		$("#destination-input").val("");
 		$("#freq-input").val("");
 		$("#arrival-input").val("");
+
+
+});
+
+
+
+// Create Firebase event for adding employee to the database and a row in the html when a user adds an entry
+database.ref().on("child_added", function(childSnapshot, prevChildKey) {
+  console.log(childSnapshot.val());
+  // Store everything into a variable.
+  var trainName = childSnapshot.val().name;
+  var trainDes = childSnapshot.val().destination;
+  var trainFreq = childSnapshot.val().frequency;
+  var trainArriv = childSnapshot.val().arrival;
+  // Employee Info
+  console.log(trainName);
+  console.log(trainDes);
+  console.log(trainFreq);
+  // console.log(trainArriv);
+  // // Prettify the employee start
+  // var empStartPretty = moment.unix(empStart).format("MM/DD/YY");
+  // // Calculate the months worked using hardcore math
+  // // To calculate the months worked
+  // var empMonths = moment().diff(moment.unix(empStart, "X"), "months");
+  // console.log(empMonths);
+  // // Calculate the total billed rate
+  // var empBilled = empMonths * empRate;
+  // console.log(empBilled);
+  // Add each train's data into the table
+  $("#train-table > tbody").append("<tr><td>" + trainName + "</td><td>" + trainDes + "</td><td>" +
+  empStartPretty + "</td><td>" + empMonths + "</td><td>" + empRate + "</td><td>" + empBilled + "</td></tr>");
 });
 
 
