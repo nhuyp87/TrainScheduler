207e31cf580aae47db649e7efe1fc3f86ee72804
nhuyp <ypham87@gmail.com>
2017-05-05 20:47:53 -0700

    initialize firebase and grab user input

--- app.js
+++ app.js
@@ -7,4 +7,17 @@
     storageBucket: "train-scheduler-b73d5.appspot.com",
     messagingSenderId: "522628858105"
   };
+
   firebase.initializeApp(config);
+
+  var database = firebase.database();
+
+// 2. Button for adding Employees
+$("#add-trainTime-btn").on("click", function(event) {
+  event.preventDefault();
+
+  // Grabs user input
+  var trainName = $("#train-name").val().trim();
+  var trainDes = $("#destination-input").val().trim();
+  var trainFreq = moment($("#freq-input").val().trim(), "Min").format("X");
+  var trainArriv = $("#arrival-input").val().trim();
