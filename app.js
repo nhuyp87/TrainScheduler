68b77a15fde40ff63110305fc3776a9cc2b92611
nhuyp <ypham87@gmail.com>
2017-05-05 20:34:36 -0700

    Updated forms

---
+++ app.js
@@ -0,0 +1,10 @@
+// Initialize Firebase
+  var config = {
+    apiKey: "AIzaSyAxOYa6CV-drR5fFHfIpDI2F5xkTJmOpVc",
+    authDomain: "train-scheduler-b73d5.firebaseapp.com",
+    databaseURL: "https://train-scheduler-b73d5.firebaseio.com",
+    projectId: "train-scheduler-b73d5",
+    storageBucket: "train-scheduler-b73d5.appspot.com",
+    messagingSenderId: "522628858105"
+  };
+  firebase.initializeApp(config);
