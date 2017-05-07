ba55dc6f8cc2899776921d7ee06c46fc360dba89
nhuyp <ypham87@gmail.com>
2017-05-06 17:15:49 -0700

    append to table changes

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -55,29 +55,35 @@ $("#add-trainTime-btn").on("click", function(event) {
 
 // Create Firebase event for adding employee to the database and a row in the html when a user adds an entry
 database.ref().on("child_added", function(childSnapshot, prevChildKey) {
-  console.log(childSnapshot.val());
-  // Store everything into a variable.
-  var trainName = childSnapshot.val().name;
-  var trainDes = childSnapshot.val().destination;
-  var trainFreq = childSnapshot.val().frequency;
-  var trainArriv = childSnapshot.val().arrival;
-  // Employee Info
-  console.log(trainName);
-  console.log(trainDes);
-  console.log(trainFreq);
-  // console.log(trainArriv);
-  // // Prettify the employee start
-  // var empStartPretty = moment.unix(empStart).format("MM/DD/YY");
-  // // Calculate the months worked using hardcore math
-  // // To calculate the months worked
-  // var empMonths = moment().diff(moment.unix(empStart, "X"), "months");
-  // console.log(empMonths);
-  // // Calculate the total billed rate
-  // var empBilled = empMonths * empRate;
-  // console.log(empBilled);
-  // Add each train's data into the table
-  $("#train-table > tbody").append("<tr><td>" + trainName + "</td><td>" + trainDes + "</td><td>" +
-  empStartPretty + "</td><td>" + empMonths + "</td><td>" + empRate + "</td><td>" + empBilled + "</td></tr>");
+  var sv = snapshot.val();  
+        
+      var svArr = Object.keys(sv);
+      var lastIndex = svArr.length - 1;
+      var lastKey = svArr[lastIndex];
+      var lastObj = sv[lastKey]
+      
+      var trainName = sv.trainName;
+      var trainDes = sv.trainDes;
+      var trainFreq = sv.trainFreq;
+      var trainArriv = sv.trainArriv;
+
+      console.log(trainName);     
+      console.log(trainDes);
+      console.log(trainFreq );
+      console.log(trainArriv);
+      var tableRow = $("<tr>").attr("data-key", sv);
+      
+      $("<td>").text(trainName).appendTo(tableRow);
+      $("<td>").text(trainDes).appendTo(tableRow);
+      $("<td>").text(trainFreq).appendTo(tableRow);
+      $("<td>").text(trainArriv).appendTo(tableRow);
+      // $("<td>").text(rate).appendTo(tableRow);
+      // $("<td>").text(totalBilled).appendTo(tableRow);
+      // $("#employees").append(tableRow);
+      // Handle the errors
+    }, function(errorObject) {
+      console.log("Errors handled: " + errorObject.code);
+    });
 });
 
 
